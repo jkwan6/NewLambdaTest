aad6d436c6037911a5a4875d444455c3d1555762
jkwan6 <91606568+jkwan6@users.noreply.github.com>
2022-04-02 18:39:17 +1100

    New Lambda test

---
+++ LambdaTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLambdaTest
+{
+    class LambdaTest
+    {
+        static public void Main(string[] args)
+        {
+
+        }
+    }
+}
