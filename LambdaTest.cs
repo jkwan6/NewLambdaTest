d797e3d865b4378ee90d5d956b284ba52e3b99cf
jkwan6 <91606568+jkwan6@users.noreply.github.com>
2022-04-02 20:07:22 +1100

    lambda stuff

--- LambdaTest.cs
+++ LambdaTest.cs
@@ -1,16 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewLambdaTest
-{
-    class LambdaTest
-    {
-        static public void Main(string[] args)
-        {
-
-        }
-    }
-}
+﻿
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLambdaTest
+{
+    class LambdaTest
+    {
+        static void Main(string[] args)
+        {
+            int x = 35;
+            int y;
+
+
+            Func<int, int> numbers = result => x * x;
+
+            y = numbers(0);
+
+            Console.WriteLine(y);
+
+            Console.ReadLine();
+        }
+    }
+}
